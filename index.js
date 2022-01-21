e3abb102a78e54e39db534ddd3e22e783dfedf1c
Ntsako Khoza <ntsako.khoza@dadevs.co.za>
2022-01-21 15:01:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app =express();
+
+app.get('/',(req,res)=>{
+    res.send('hello world');
+});
+
+app.get('/api/courses',(req, res)=>{
+    res.send[1,2,3];
+});
+
+// API/courses/1
+
+//port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
