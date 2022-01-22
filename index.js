b604393b69237dcfb52554c2c52f1f8b4b73734a
ntsakokhoza <ntsako.khoza@dadevs.co.za>
2022-01-23 01:55:23 +0200

    initial commit

--- index.js
+++ index.js
@@ -3,18 +3,40 @@ const express = require('express');
 const res = require('express/lib/response');
 const app =express();
 
+app.use(express.json());
+
+const courses =[
+    {id:1, name: "couse1"},
+    {id:2, name: "couse2"},
+    {id:3, name: "couse3"}
+];
 app.get('/',(req,res)=>{
     res.send('hello world');
 });
 
 app.get('/api/courses',(req, res)=>{
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
+app.post('/api/courses',(req,res) =>{
+    const course = {
+        id: courses.length+1,
+        name: req.body.name
+    };
+    course.push(course)
+    res.send(course);
+})
+
 // API/courses/1
-app.get('/api/posts/:year/:month',(req,res)=>{
-    res.send(req.params.year);
+app.get('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course){
+        res.status(404).send("The course with the given ID was not found.");
+    }
+    res.send(course)
+
 });
+
 
 //port
 const port = process.env.PORT || 3000;
