81ed552ebfb070f2a982dcac87f99ee081daf4e1
ntsakokhoza <ntsako.khoza@dadevs.co.za>
2022-01-23 02:37:53 +0200

    ft-http post request to add a course with input validation with Joi

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const { application } = require('express');
 const express = require('express');
 const res = require('express/lib/response');
@@ -6,10 +7,11 @@ const app =express();
 app.use(express.json());
 
 const courses =[
-    {id:1, name: "couse1"},
-    {id:2, name: "couse2"},
-    {id:3, name: "couse3"}
+    {id:1, name: "course1"},
+    {id:2, name: "course2"},
+    {id:3, name: "course3"}
 ];
+
 app.get('/',(req,res)=>{
     res.send('hello world');
 });
@@ -19,9 +21,16 @@ app.get('/api/courses',(req, res)=>{
 });
 
 app.post('/api/courses',(req,res) =>{
-    if(!req.body.name || req.body.name.length<3){
+    const schema = {
+        name:Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema)
+
+
+    if(result.error){
         //400 Bad Request
-        res.status(400).send("Name is required and should be minimum 3 charactors")
+        res.status(400).send(result.error.details[0].message);
         return;
     }
     const course = {
@@ -41,6 +50,16 @@ app.get('/api/courses/:id',(req,res)=>{
     res.send(course)
 
 });
+
+// app.put('/app/courses/:id', (req,res) =>{
+//     const course = courses.find(c => c.id === parseInt(req.params.id));
+//     if(!course){
+//         res.status(404).send("The course with the given ID was not found.");
+//     }
+    
+
+
+// })
 
 
 //port
